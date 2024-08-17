7fb8b3e62f387d93b791f47b379313e18fd418ac
Student10 <eashan.bmvb@gmail.com>
2024-08-17 13:03:37 +0530

    final commit of the day

--- quiz-app/src/app/components/HomePage.jsx
+++ quiz-app/src/app/components/HomePage.jsx
@@ -1,12 +1,25 @@
 import Card from './Card'
+import './HomePage.css'
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='cardbox'>
         <Card title={'Physics'} backgroundColor={'blue'} info={
             ''
         }
-        color1={'#8dc1fc'} color2={'#4c5fed'}
+        color1={'#8dc1fc'} color2={'#1c33d9'}
+        />
+
+        <Card title={'Chemistry'} backgroundColor={'blue'} info={
+            ''
+        }
+        color1={'#8dc1fc'} color2={'#1c33d9'}
+        />
+
+        <Card title={'Maths'} backgroundColor={'blue'} info={
+            ''
+        }
+        color1={'#8dc1fc'} color2={'#1c33d9'}
         />
     </div>
   )
